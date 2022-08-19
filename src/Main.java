d4cde001ad56cbcad0a3c0a4ba7e8d63559d38b3
Alexis-Mendez2812 <281212.namaste@gmail.com>
2022-08-19 16:58:55 -0300

    Ejercicios tema 4

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,55 @@ public class Main {
 
         System.out.println(miCoche.puertas);
         System.out.println(resultado);
+        var numeroIf = 0;
+
+    if(numeroIf>0){
+        System.out.println("numeroIf es positivo");
+    }else if(numeroIf<0){
+        System.out.println("numeroIf es negativo");
+    }else {
+        System.out.println("numeroIf es " + numeroIf );
+        }
+    int numeroWhile = 0;
+
+    while(numeroWhile<3){
+        numeroWhile++;
+        System.out.println(numeroWhile);
+    }
+
+        do{
+            numeroWhile++;
+            System.out.println(numeroWhile);
+        }while(numeroWhile<3);
+
+        int numeroFor = 0;
+        for (int i = 0; i <=3 ; i++) {
+            numeroFor++;
+            System.out.println(numeroFor);
+        }
+
+
+        var estacion = "LUNA";
+
+    switch (estacion){
+        case "VERANO":
+            System.out.println("es verano");
+            break;
+        case "INVIERNO":
+            System.out.println("es invierno");
+            break;
+        case "PRIMAVERA":
+            System.out.println("es primavera");
+            break;
+        case "OTOÑO":
+            System.out.println("es otoño");
+            break;
+        default:
+            System.out.println("no es estacion" );
+    }
+
+
+
     }
 
     public static int suma(int a,int b,int c) {
